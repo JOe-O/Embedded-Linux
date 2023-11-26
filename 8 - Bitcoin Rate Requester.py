4ceeeea03aeb4d616b8faf9df429a988f52eddf2
JOe-O <yousefosama3@gmail.com>
2023-11-26 19:15:07 +0200

    Task 8 - Python

---
+++ 8 - Bitcoin Rate Requester.py
@@ -0,0 +1,19 @@
+'''
+Write code to find automatically bitcoin rate
+'''
+import requests
+import json
+
+#API Endpoint
+url = "https://api.coindesk.com/v1/bpi/currentprice.json"
+
+#A GET request to the API
+response = requests.get(url)
+
+#Convert Response to json
+response_json = response.json()
+# print(response_json)
+
+#Format the json output for better visuals
+pretty_json = json.dumps(response_json, indent=4)
+print(pretty_json)
